e213122e8dfb0a2cbe391a6138f376d1c59fbbd2
RJSIN147 <rjsinha2217@gmail.com>
2026-08-18 17:15:08 +0530

    input and variable script

---
+++ ses-3/input.sh
@@ -0,0 +1,5 @@
+read -p "Enter you name:" name
+read -p "Enter your roll:" roll
+read -p "Enter your comment:" comment
+
+echo "My name is $name, my roll is $roll, and comment is $comment"
---
+++ ses-3/variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+name=Ravijot
+roll=307
+comment="Devops script"
+
+echo "My name is $name, my roll no. is $roll and this is a $comment"
